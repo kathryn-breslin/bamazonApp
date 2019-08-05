f304a8a0f0444100a411300faf67257456ff10ba
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-05 18:20:51 -0400

    add new department into departments table

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -22,14 +22,18 @@ function afterConnection() {
             name: "supervisorQuestions",
             type: "list",
             message: "Please select an option to continue.",
-            choices: ["View Product Sales by Department", "Create New Department"]
+            choices: ["View Product Sales by Department", "Create New Department", "EXIT"]
         }
-    ).then(function(answer) {
-        if (answer.supervisorQuestions === "View Product Sales by Department"){
+    ).then(function (answer) {
+        if (answer.supervisorQuestions === "View Product Sales by Department") {
             showSales();
         }
+        else if (answer.supervisorQuestions === "Create New Department") {
+            createDepartment();
+        }
         else {
-            // createDepartment();
+            console.log("Exiting the system.");
+            connection.end();
         }
     })
 }
@@ -40,12 +44,42 @@ var table = new Table({
 });
 
 function showSales() {
-    connection.query("SELECT departments.department_id, departments.department, departments.over_head_costs, SUM(products.product_sales) as product_sales, SUM(products.product_sales) - departments.over_head_costs as total_profit FROM products INNER JOIN departments ON products.department = departments.department GROUP BY departments.department_id, departments.department, departments.over_head_costs", function(err, res) {
+    connection.query("SELECT departments.department_id, departments.department, departments.over_head_costs, SUM(products.product_sales) as product_sales, SUM(products.product_sales) - departments.over_head_costs as total_profit FROM products INNER JOIN departments ON products.department = departments.department GROUP BY departments.department_id, departments.department, departments.over_head_costs", function (err, res) {
         if (err) throw err;
         // console.log(res);
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].department_id, res[i].department, res[i].over_head_costs, res[i].product_sales, res[i].total_profit])
         }
+        console.log("")
         console.log(table.toString());
+        console.log("")
+        console.log("")
+        afterConnection();
     });
+}
+
+function createDepartment() {
+    inquirer.prompt([
+        {
+            name: "newDepartment",
+            type: "input",
+            message: "What is the name of the Department you would like to create?"
+        },
+        {
+            name: "newOverhead",
+            type: "input",
+            message: "What is the overhead cost for this department?"
+        }
+    ]).then(function (answer) {
+        console.log(answer.newDepartment)
+        connection.query("INSERT INTO departments (department, over_head_costs) VALUES (?, ?)",
+            [answer.newDepartment, answer.newOverhead],
+            function (err, res) {
+                if (err) throw err;
+                console.log("You have now added " + answer.newDepartment + " with an Overhead cost of $" + answer.newOverhead + " to the database.")
+                console.log("")
+                console.log("")
+                afterConnection();
+            })
+    })
 }
